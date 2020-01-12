5fe9909425146ccebd132b1872d8c84b38eada0c
Zainab Jimoh <jimohzainab4753@gmail.com>
2020-01-12 16:30:29 +0000

    Implemented chaining and introduced button to change the colours of the boxes

--- app/src/main/java/com/zennymorh/colormyviews/MainActivity.kt
+++ app/src/main/java/com/zennymorh/colormyviews/MainActivity.kt
@@ -16,7 +16,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners(){
-        val clickableViews : List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout)
+        val clickableViews : List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text, constraint_layout,
+            red_button, yellow_button, green_button)
 
         for (item in clickableViews){
             item.setOnClickListener { makeColoured(it) }
@@ -32,7 +33,13 @@ class MainActivity : AppCompatActivity() {
             // Boxes using Android color resources for background
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_blue_dark)
-            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_red_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_purple)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
